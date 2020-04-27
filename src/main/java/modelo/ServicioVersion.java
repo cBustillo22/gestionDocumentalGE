a0e57abea0d1ad58e6f21591cf951ded2d426582
cbustillo22 <carlosj99.cjbl@gmail.com>
2020-04-27 18:28:05 -0500

    crud versiones terminado

--- src/main/java/modelo/ServicioVersion.java
+++ src/main/java/modelo/ServicioVersion.java
@@ -1,13 +1,16 @@
 package modelo;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import conexion.Conexion;
 import estructural.Carpeta;
 import estructural.Documento;
 import estructural.Version;
 
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class ServicioVersion {
@@ -45,6 +48,94 @@ public class ServicioVersion {
         return respuesta;
     }
 
+    public boolean eliminar(Version versionDocuemnto, Documento documentoRaiz){
+        boolean resultado = false;
+        try {
+            ApiFuture<WriteResult> writeResult = conexion.getDb().collection("carpetas")
+                    .document(documentoRaiz.getCarpetaRaiz())
+                    .collection("archivos")
+                    .document(versionDocuemnto.getDocumentoRaiz())
+                    .collection("versiones")
+                    .document(Integer.toString(versionDocuemnto.getVersion()))
+                    .delete();
+            System.out.println("Update time : " + writeResult.get().getUpdateTime());
+            resultado = true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public boolean actualizar(Version versionDocumento, Documento documentoRaiz){
+        boolean respuesta = false;
+        try {
+            ApiFuture<WriteResult> future = conexion.getDb().collection("carpetas")
+                    .document(documentoRaiz.getCarpetaRaiz())
+                    .collection("archivos")
+                    .document(versionDocumento.getDocumentoRaiz())
+                    .collection("versiones")
+                    .document(Integer.toString(versionDocumento.getVersion()))
+                    .set(versionDocumento);
+            System.out.println("Update time : " + future.get().getUpdateTime());
+            respuesta=true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return respuesta;
+    }
+
+    public Version buscarPorVersion(Version versionDocumento, Documento documentoRaiz){
+        Version versionEncontrada = null;
+        try {
+            DocumentReference docRef = conexion.getDb().collection("carpetas")
+                    .document(documentoRaiz.getCarpetaRaiz())
+                    .collection("archivos")
+                    .document(versionDocumento.getDocumentoRaiz())
+                    .collection("versiones")
+                    .document(Integer.toString(versionDocumento.getVersion()));
+            ApiFuture<DocumentSnapshot> future = docRef.get();
+            DocumentSnapshot document = future.get();
+            if (document.exists()) {
+                versionEncontrada = new Version(null,
+                        Integer.parseInt(document.get("version").toString()),
+                        document.getString("base64File"),
+                        document.getBoolean("esVersionMayor"),
+                        document.getString("documentoRaiz")
+                        );
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return versionEncontrada;
+    }
+
+    public ArrayList<Version> listar(Documento documento){
+        ArrayList<Version> respuesta = new ArrayList<>();
+        try {
+            ApiFuture<QuerySnapshot> query = conexion.getDb().collection("carpetas")
+                    .document(documento.getCarpetaRaiz())
+                    .collection("archivos")
+                    .document(documento.getNombre())
+                    .collection("versiones")
+                    .get();
+            QuerySnapshot querySnapshot = query.get();
+            List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                Version versionDocumento = new Version(null,
+                        Integer.parseInt(document.get("version").toString()),
+                        document.getString("base64File"),
+                        document.getBoolean("esVersionMayor"),
+                        document.getString("documentoRaiz")
+                );
+                respuesta.add(versionDocumento);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return respuesta;
+    }
+
+
     public static void main(String[] args) {
         ServicioDocumento servicioDocumento = new ServicioDocumento();
         ServicioCarpeta servicioCarpeta = new ServicioCarpeta();
@@ -55,9 +146,11 @@ public class ServicioVersion {
         doc.setCarpetaRaiz(carpeta.getNombre());
         Documento doc1 = servicioDocumento.buscarPorNombre(doc);
         //Documento doc = new Documento(carpeta, "facturaAbril.pdf","administrador2",742.15,"pdf", "factura del servicio del agua mes de abril", new Date(), "ACTIVO");
-        String b64file = "b64test123";
-        Version ver = new Version(UUID.randomUUID(), 1, b64file, true, doc1.getNombre());
-        servicioVersion.agregar(ver, doc1);
+        String b64file = JOptionPane.showInputDialog("a");
+        Version ver = new Version(UUID.randomUUID(), 2, b64file, true, doc1.getNombre());
+        //Version ver1 = servicioVersion.buscarPorVersion(ver, doc1);
+        ArrayList<Version> lista = servicioVersion.listar(doc1);
+        System.out.println(lista);
     }
 
 }
